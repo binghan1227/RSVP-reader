a24e25e8cc67fa31d627b895d04e24211d37fe46
binghan1227 <binghan1227@126.com>
2026-01-20 20:18:48 +0800

    follow the chorme extension policy

---
+++ extension/popup-init.js
@@ -0,0 +1,7 @@
+// Initialize RSVP controller with extension-specific options
+initRSVPController({
+  fontSize: '36px',
+  disableStartOnPlay: true,
+  throttleSpeed: true,
+  defaultText: 'The quick brown fox jumps over the lazy dog.'
+});
